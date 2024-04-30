3e7df23e001b3257f0db1094b583f087d2fe7c78
declannchong <declannchongmf.23@ichat.sp.edu.sg>
2024-04-30 13:26:40 +0800

    Confirgured to Lab2

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(calculate_bmi))
     if calculate_bmi < 18.5:
         print("You are underweight")
+        return -1
     elif 18.5 <= calculate_bmi <= 25.0:
         print("You are normal weight")
+        return 0
     else:
         print('You are overweight')
+        return 1
 
       
 calculate_bmi(weight = 60, height = 1.73)
